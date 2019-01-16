44425c73d147c67474b8f31f5e7fbb13709c92f0
Ayaz.Ayupov <ayaz.ayupov@gmail.com>
2019-01-16 22:07:56 +0300

    added function for starting migration functions

---
+++ migrations/migrate.go
@@ -0,0 +1,27 @@
+package migrations
+
+
+import (
+	"go-web/db"
+)
+
+func StartMigration() {
+	connect, err := getDbConnection()
+	if err != nil {
+		panic(err)
+	}
+	createTables(connect)
+}
+
+func RollBack() {
+	connect, err := getDbConnection()
+	if err != nil {
+		panic(err)
+	}
+	dropTables(connect)
+}
+
+func getDbConnection() (*db.DB, error) {
+	dbConfig := db.GetConfigs()
+	return db.DbConnection(dbConfig)
+}
