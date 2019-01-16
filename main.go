226c5a6a761b8b73dcf703b40e71abce88c0f652
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-16 17:25:43 +0300

    tmp

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main(){}
+import (
+	"go-web/daemon"
+	"go-web/db"
+)
+
+func main() {
+	dbConfig := db.Config{
+		Driver: "mysql",
+		User: "tommy",
+		Password: "43",
+		Host: "127.0.0.1",
+		Database: "go_lang",
+		Port: 3306,
+	}
+
+	daemonConfig := daemon.Config{
+		ListenSpec: "localhost:8000",
+		Db: dbConfig,
+	}
+
+	daemon.Run(&daemonConfig)
+}
