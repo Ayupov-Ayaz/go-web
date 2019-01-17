0c61041571ee73d42131064ec3b2c4cb96e80b75
Аюпов Аяз <ayaz.ayupov@gmail.com>
2019-01-17 22:04:20 +0300

    fix: run main.go with flags for migrations

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ var assetsPath string
 
 func processFlags() * daemon.Config {
 	cfg := &daemon.Config{}
-	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
 	cfg.Db = &db.Config{ Driver: "mysql", User: "tommy", Password: "43", Host: "127.0.0.1", Database: "go_lang", Port: 3306}
+	//// Если main запущен с аргументами, то запускаем наши миграции
+	if len(os.Args) > 1 {
+		migrations.Migration(os.Args[1], cfg.Db)
+		os.Exit(-1)
+	}
+	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
 	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to static dir")
 	flag.Parse()
 	return cfg
@@ -27,11 +32,6 @@ func setupHttpAssets(cfg *daemon.Config) {
 func main() {
 	cfg := processFlags()
 	setupHttpAssets(cfg)
-	//// Если main запущен с аргументами, то запускаем наши миграции
-	if len(os.Args) > 1 {
-		migrations.Migration(os.Args[1], cfg.Db)
-		os.Exit(-1)
-	}
 
 	if err := daemon.Run(cfg); err != nil {
 		log.Printf("Error in main: \n %s", err.Error())
