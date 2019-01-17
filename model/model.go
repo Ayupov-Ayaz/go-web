fe2f05cc496d0d66d707be701c1f3bbb1dcacbb7
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-17 17:20:23 +0300

    changed model.Model

--- model/model.go
+++ model/model.go
@@ -1,27 +1,17 @@
 package model
 
-import (
-	"go-web/comands"
-	"go-web/db"
-)
-
-// структура которая будет иметь подключение к бд и будет выступать в качестве CRUD
-// для наших структур хранящихся в бд
 type Model struct {
-	db.DB // расширяем бд
+	db
 }
 
-func New(db db.DB) *Model{
+
+func New(db db) *Model{
 	return &Model{
-		db,
+		db: db,
 	}
 }
 
-func SelectAllUsers() []*User{
-	userCommands := comands.UserCommands{}
-	users, err := userCommands.GetUsers()
-	if err != nil {
-		panic(err)
-	}
-	return users
+func (m *Model)SelectAllUsers() ([]*User, error){
+
+	return nil, nil
 }
