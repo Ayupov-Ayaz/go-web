c711656ba600301f25d94bde22adeb83d473be7b
Ayaz.Ayupov <ayaz.ayupov@gmail.com>
2019-01-20 23:58:06 +0300

    added post mutations

---
+++ graphQl/mutation/post_mutations.go
@@ -0,0 +1,33 @@
+package mutation
+
+import (
+	"github.com/Ayupov-Ayaz/go-web/db"
+	"github.com/Ayupov-Ayaz/go-web/graphQl/types"
+	"github.com/Ayupov-Ayaz/go-web/model"
+	"github.com/graphql-go/graphql"
+)
+
+// Создание post
+func CreatePost(db *db.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: types.GetQueryPostType(),
+		Description: "Create new Post",
+		Args: graphql.FieldConfigArgument{
+			// перечисляем наши поля
+			"title" 	  : types.GqNewNonNullString(),
+			"description" : types.GqNewNonNullString(),
+			"author" 	  : types.GqNewNonNullInt(),
+
+		},
+		Resolve: func(p graphql.ResolveParams) (i interface{}, e error) {
+			post := &model.Post{
+				Title: p.Args["title"].(string),
+				Description: p.Args["description"].(string),
+				Author: p.Args["author_id"].(int64),
+			}
+			return db.InsertPost(post)
+		},
+	}
+}
+
+
