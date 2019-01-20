f97edaa7294d6a54fe9046733f363f923262109f
Ayaz.Ayupov <ayaz.ayupov@gmail.com>
2019-01-20 23:39:58 +0300

    using GQL in ui, added function executorQuery

--- ui/ui.go
+++ ui/ui.go
@@ -1,8 +1,12 @@
 package ui
 
 import (
+	"fmt"
 	"github.com/Ayupov-Ayaz/go-web/db"
+	"github.com/Ayupov-Ayaz/go-web/graphQl"
 	"github.com/Ayupov-Ayaz/go-web/model"
+	"github.com/graphql-go/graphql"
+	"log"
 	"net"
 	"net/http"
 	"time"
@@ -20,14 +24,23 @@ func Start(cfg *Config, db *db.DB,  listner *net.Listener) {
 		MaxHeaderBytes: 1 << 16}
 
 	http.Handle("/assets/", http.StripPrefix(cfg.AssetsPrefix, http.FileServer(http.Dir(cfg.AssetsPath))))
-	m := model.New(db)
 
-	routes(m)
+	m := model.New(db)
+	schema := graphQl.NewGraphQlSchema(db)
+
+	routes(m, schema)
 	server.Serve(*listner)
 }
 
-func routes(m *model.Model) {
-	http.Handle("/", IndexHandler(m))
-	http.Handle("/sign/up", ShowRegisterFormHandler())
-	http.Handle("/registration", RegisterHandler(m))
+
+func executorQuery(query string, schema graphql.Schema) *graphql.Result {
+	fmt.Println(query, schema)
+	result := graphql.Do(graphql.Params{
+		Schema: schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		log.Printf("ERROR |main/executorQuery: %v ", result.Errors)
+	}
+	return result
 }
