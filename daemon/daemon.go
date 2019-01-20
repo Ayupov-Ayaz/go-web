3ee44c4481cb2410d8aca658835b49e303928b91
Ayaz.Ayupov <ayaz.ayupov@gmail.com>
2019-01-20 23:35:01 +0300

    deleted func waitSignal id daemon

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -6,9 +6,6 @@ import (
 	"github.com/Ayupov-Ayaz/go-web/ui"
 	"log"
 	"net"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 type Config struct {
@@ -31,11 +28,4 @@ func Run(cfg *Config) {
 	}
 	fmt.Printf("Starting server on %s \n", cfg.ListenSpec )
 	ui.Start(cfg.UI, db, &l)
-}
-
-func waitForSignal() {
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	s := <- ch
-	log.Printf("Got signal: %v, exiting.", s)
 }
