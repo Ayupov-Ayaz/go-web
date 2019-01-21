c0dec16d70fa0030d00437e7dbf3042f7ab84cd5
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-21 09:37:18 +0300

    added functions for working with type Post in db interface

--- model/db.go
+++ model/db.go
@@ -1,7 +1,14 @@
 package model
-
+/**
+ 	All queries for all types
+ */
 type db interface {
+	// user
 	SelectAllUsers() (*[]User, error)
 	SelectUserById(id int64) (*User, error)
 	InsertUser(user * User) (lastId int64, err error)
+
+	// post
+	InsertPost(post *Post) (lastId int64, err error)
+	SelectPostById(id int64) (*Post, error)
 }
