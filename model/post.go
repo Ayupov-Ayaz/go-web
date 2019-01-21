0fd41c838582dc778f377f120aa74d7b122fe5c5
Аяз Аюпов <ayaz.ayupov@gmail.com>
2019-01-21 09:37:56 +0300

    added json annotation for structure Post

--- model/post.go
+++ model/post.go
@@ -1,8 +1,8 @@
 package model
 
 type Post struct {
-	ID int64
-	Title string
-	Description string
+	ID int64 `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
 	Author int64 `db:"author_id" json:"author_id"`
 }
