3b01c7d07978127e0393d1be5513c78724beb0c6
Ayaz.Ayupov <ayaz.ayupov@gmail.com>
2019-02-03 23:34:47 +0300

    added type Errors

--- errors/errors.go
+++ errors/errors.go
@@ -9,3 +9,17 @@ func PrintSystemErr(message string) {
 	log.Println("| SYSTEM.ERROR |" + message)
 	os.Exit(-1)
 }
+
+type Error struct {
+	s string
+}
+
+func NewError(message string) *Error {
+	return &Error{
+		s: message,
+	}
+}
+
+func (e *Error)Error() string {
+	return e.s
+}
